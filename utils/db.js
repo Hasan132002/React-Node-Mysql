167225755ba32df59e482ff279f929080a3fb851
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2024-12-30 22:23:15 +0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,8 @@ const con = mysql.createConnection({
     host: "mysql.railway.internal",
     user: "root",
     password: "qwpzEAgQvxcZHgiMLAmeDeBrPTusJloJ",
-    database: "railway"
+    database: "railway",
+    port: 3306,
 })
 
 con.connect(function(err) {
