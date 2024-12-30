eba017f3dbe84a1f00e4787026f93a6ebe2e8f21
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2024-12-30 22:54:50 +0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,18 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production'; 
+const host = isProduction ? process.env.MYSQL_HOST : process.env.MYSQL_PUBLIC_URL; 
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql.railway.internal',
-    user: 'root',
-    password: 'qwpzEAgQvxcZHgiMLAmeDeBrPTusJloJ',
-    database: 'railway',
-    port: 3306
+    host: host,  // Use internal or public URL
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT || 3306
 });
 
 pool.getConnection((err, connection) => {
@@ -15,7 +21,7 @@ pool.getConnection((err, connection) => {
         return;
     }
     console.log("Connected to the database");
-    connection.release();  
+    connection.release();  // Release the connection after use
 });
 
 export default pool;
