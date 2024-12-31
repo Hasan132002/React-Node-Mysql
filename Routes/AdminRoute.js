b0a24dd862bb2f6f82382a2cc92b6189ac091a63
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2024-12-31 21:31:39 +0500

    Update AdminRoute.js

--- Routes/AdminRoute.js
+++ Routes/AdminRoute.js
@@ -18,7 +18,9 @@ const corsOptions = {
 };
 router.use(cors(corsOptions)); 
 router.post("/adminlogin", (req, res) => {
-  const sql = "SELECT * from admin Where email = ? and password = ?";
+  const sql = "SELECT * from ADMIN Where email = ? and password = ?";
+  console.log("sss0",sql);
+
   con.query(sql, [req.body.email, req.body.password], (err, result) => {
     if (err) return res.json({ loginStatus: false, Error: "Query error" });
     if (result.length > 0) {
