c57adfe105f958276e327e8bfeeedb6863752d4b
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2024-12-31 22:13:21 +0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,17 @@
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 console.log('sss');
 
-const isProduction = process.env.NODE_ENV === 'production'; 
+const isProduction = process.env.NODE_ENV === 'production';
 
 const host = isProduction ? process.env.MYSQL_HOST : process.env.MYSQL_PUBLIC_URL;
 console.log(host);
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'roundhouse.proxy.rlwy.net',  
+    host: 'roundhouse.proxy.rlwy.net',
     user: 'root',
     password: 'BSwrdhaoxyajrWXRuTqmGrajFafHUKur',
     database: 'railway',
@@ -19,13 +19,13 @@ const pool = mysql.createPool({
 });
 console.log(pool);
 
-pool.getConnection((err, connection) => {
-    if (err) {
+pool.getConnection()
+    .then(connection => {
+        console.log("Connected to the database");
+        connection.release();
+    })
+    .catch(err => {
         console.log("Error connecting to the database:", err.message);
-        return;
-    }
-    console.log("Connected to the database");
-    connection.release(); 
-});
+    });
 
 export default pool;
