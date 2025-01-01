202d2defd1aa9ab545dcf383420b623b76e44db3
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2025-01-01 23:11:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://react-node-mysql.vercel.app"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }));
